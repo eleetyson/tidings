e804413e403bb77ad4013f28c0184aada0733c68
Ethan <eleetyson@gmail.com>
2020-12-18 20:53:02 -0500

    cloudinary script tag must be at bottom of body

--- src/components/ImageUploadForm.js
+++ src/components/ImageUploadForm.js
@@ -1,21 +1,36 @@
 // a simple image upload component to handle user image selection
 import React from 'react'
-import ImageUploader from 'react-images-upload'
+// import ImageUploader from 'react-images-upload'
 
 export default function ImageUploadForm(props) {
+  const widget = cloudinary.createUploadWidget(
+    { cloudName: "df7waillu", uploadPreset: "tidings" },
+    (error, result) => { console.log(error, result) }
+  )
+
+  const openWidget = () => {
+    widget.open()
+  }
+
   return (
     <div className="box-shadow-card py-2 px-3">
       <p className="box-shadow-card-title">1. Upload an image</p>
 
-      <ImageUploader
-        {...props}
-        onChange={props.handleUpload}
-        label={null} withIcon={true} singleImage={true} withPreview={true}
-        buttonText="Upload"
-        imgExtension={[".jpg", ".png"]}
-      />
+      <button type="button" id="upload_widget" className="btn btn-success" onClick={openWidget}>
+        Upload image
+      </button>
 
     </div>
   )
 
 }
+
+// <p className="box-shadow-card-title">1. Upload an image</p>
+//
+// <ImageUploader
+//   {...props}
+//   onChange={props.handleUpload}
+//   label={null} withIcon={true} singleImage={true} withPreview={true}
+//   buttonText="Upload"
+//   imgExtension={[".jpg", ".png"]}
+// />
