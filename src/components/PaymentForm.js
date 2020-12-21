0327d500ddfc61bee2eafc02fc56f21082ae8273
Ethan <eleetyson@gmail.com>
2020-12-21 08:59:01 -0500

    loading stripe module for checkout form

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_KEY)
 
 export default function PaymentForm() {
   return (
     <div className="box-shadow-card py-2 px-3">
       <p className="box-shadow-card-title">3. Send your postcard</p>
+
+
+
     </div>
   )
 
