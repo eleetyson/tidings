063d7a1a24a256357de5456ef65f48ce001c9739
Ethan <eleetyson@gmail.com>
2020-12-21 10:17:49 -0500

    styling payment form

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -5,29 +5,34 @@ import { loadStripe } from '@stripe/stripe-js'
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_KEY)
 
 export default function PaymentForm() {
+
+  const handleSubmit = event => {
+    console.log("in form submit event handler")
+    event.preventDefault()
+  }
+
   return (
     <div className="box-shadow-card  py-2 px-3">
       <p className="box-shadow-card-title">3. Send your postcard</p>
 
       <Elements stripe={stripePromise}>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: '16px',
-                color: '#424770',
-              },
-              invalid: {
-                color: '#9e2146',
-              },
-            }
-          }}
-        >
-        </CardElement>
+        <form onSubmit={handleSubmit}>
 
-        <button className="paymentBtn  btn btn-lg btn-block btn-success  border-0" type="button" disabled={false}>
-          Pay $1
-        </button>
+          <CardElement
+            options={{
+              style: {
+                base: { fontSize: '16px', color: '#424770' },
+                invalid: { color: '#9e2146' }
+              }
+            }}
+          >
+          </CardElement>
+
+          <button className="paymentBtn  btn btn-lg btn-block btn-success  border-0" type="submit" disabled={true}>
+            Pay $1
+          </button>
+
+        </form>
       </Elements>
 
     </div>
